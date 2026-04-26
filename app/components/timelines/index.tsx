01991deb48c60971f5c77bda365cfddbbcea1042
devproje <me@projecttl.net>
2026-04-26 14:32:57 +0900

    feat: update timeline item

--- app/components/timelines/index.tsx
+++ app/components/timelines/index.tsx
@@ -1,5 +1,3 @@
-import { Calendar } from "lucide-react";
-
 export type TimelineType = "education" | "hackathon" | "conference" | "certificate";
 
 export interface TimelineData {
@@ -17,100 +15,20 @@ interface TimelinesProps {
 	timelines: TimelineData[];
 }
 
-function RenderDate({ date }: { date: Date }) {
-	let month = "";
-	let day = `${date.getDate()}`;
-	let d = "";
-
-	switch (date.getMonth()) {
-		case 0:
-			month = "Jan";
-			break;
-		case 1:
-			month = "Feb";
-			break;
-		case 2:
-			month = "Mar";
-			break;
-		case 3:
-			month = "Apr";
-			break;
-		case 4:
-			month = "May";
-			break;
-		case 5:
-			month = "Jun";
-			break;
-		case 6:
-			month = "Jul";
-			break;
-		case 7:
-			month = "Aug";
-			break;
-		case 8:
-			month = "Sep";
-			break;
-		case 9:
-			month = "Oct";
-			break;
-		case 10:
-			month = "Nov";
-			break;
-		case 11:
-			month = "Dec";
-		}
-
-	switch (date.getDay()) {
-		case 0:
-			d = "Sun";
-			break;
-		case 1:
-			d = "Mon";
-			break;
-		case 2:
-			d = "Tue";
-			break;
-		case 3:
-			d = "Wed";
-			break;
-		case 4:
-			d = "Thu";
-			break;
-		case 5:
-			d = "Fri";
-			break;
-		case 6:
-			d = "Sat";
-	}
-
-	if (date.getDate() < 10)
-		day = `0${date.getDate()}`;
-
-	return (
-		<>
-			<Calendar size={20} />
-
-			{month} {day}, {d}
-			<b className="font-bold">{date.getFullYear()}</b>
-		</>
-	);
-}
-
 function Timeline(props: TimelineProps) {
 	return (
-		<div className="flex flex-row justify-between items-center">
-			<div className="flex flex-row items-center justify-center gap-1.5 h-5">
-				<div className="flex flex-row gap-x-1.5 my-2 text-sm cursor-default">
-					<span className="bg-blue-500 px-3 rounded-full text-foreground">
+		<div className="flex flex-col items-start gap-1 sm:flex-row sm:justify-between sm:items-center">
+			<div className="flex flex-row items-center justify-center gap-1.5 h-5 w-fit">
+				<span className="text-sm text-foreground">[{props.timeline.date.getFullYear()}]</span>
+				<span className="text-sm items-center">{props.timeline.title}</span>
+			</div>
+
+			<div className="flex flex-row items-center gap-1.5 sm:h-5">
+				<div className="flex flex-row gap-x-1.5 my-2 cursor-default">
+					<span className="bg-blue-500 px-3 rounded-full text-foreground text-sm">
 						{props.timeline.type.charAt(0).toUpperCase() + props.timeline.type.substring(1, props.timeline.type.length)}
 					</span>
 				</div>
-
-				{props.timeline.title}
-			</div>
-
-			<div className="flex flex-row items-center gap-1.5 h-5">
-				<RenderDate date={props.timeline.date} />
 			</div>
 		</div>
 	);
@@ -118,7 +36,7 @@ function Timeline(props: TimelineProps) {
 
 export default function Timelines(props: TimelinesProps) {
 	return (
-		<div className="flex flex-col-reverse gap-y-2">
+		<div className="flex flex-col-reverse gap-y-4">
 			{props.timelines.map((data, index) => <Timeline timeline={data} key={index} />)}
 		</div>
 	);
