835478416cebb4d5af16cd3352fc87c9a5898c92
Project_IO <me@projecttl.net>
2026-05-13 12:21:43 +0900

    style: fix head align

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import { SocialRow } from "./social-row";
 
 export default function Header() {
 	return (
-		<header className="flex flex-col">
+		<header className="flex flex-col w-full">
 			<div className="flex items-center">
 				<img className="mr-2.5" src="/logo.png" width={40} height={40} alt="" loading="eager" />
 				<span className="email-addr h-10 content-center">
