d466aeafe417aa5ee7e7aa38ea5e5eb0cf48370f
devproje <me@projecttl.net>
2026-05-15 18:51:17 +0900

    feat: notfound page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "@/App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import NotFound from "./notfound";
 
 const router = (
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<App />} index />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	</BrowserRouter>
 );
