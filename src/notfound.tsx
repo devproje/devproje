7925e0c86cef7477a79f8eb18a23af296d39b48b
devproje <me@projecttl.net>
2026-05-15 18:54:55 +0900

    fix: image url resolve

--- src/notfound.tsx
+++ src/notfound.tsx
@@ -2,7 +2,7 @@ function NotFound() {
 	return (
 		<div className="notfound m-20 text-center">
 			{/* Temporary image */}
-			<img className="notfound-logo" src="/public/logo.png" />
+			<img className="notfound-logo" src="/logo.png" />
 			<h2 className="mt-1 mb-1 text-[2rem] font-extrabold">404 Not Found</h2>
 			<p className="mb-10">Current page is not available.</p>
 
